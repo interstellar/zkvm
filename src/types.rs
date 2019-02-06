45db57ffd3d5dd7b69793425b1e99e20f365ecd7
BlockChainDev <kevtheappdev@gmail.com>
2019-02-06 22:25:10 +0100

    Added back to_point using new api

--- src/types.rs
+++ src/types.rs
@@ -270,6 +270,28 @@ impl Data {
             },
         }
     }
+
+    /// Converts a bytestring to a canonical scalar
+    pub fn to_scalar(self) -> Result<Scalar, VMError> {
+        match self {
+            Data::Opaque(data) => {
+                let scalar = Subslice::new(&data).read_scalar()?;
+                Ok(scalar)
+            }
+            Data::Witness(_) => unimplemented!(),
+        }
+    }
+
+    /// Converts a bytestring to a compressed point
+    pub fn to_point(self) -> Result<CompressedRistretto, VMError> {
+        match self {
+            Data::Opaque(data) => {
+                let point = Subslice::new(&data).read_point()?;
+                Ok(point)
+            }
+            Data::Witness(_) => unimplemented!(),
+        }
+    }
 }
 
 impl Contract {
